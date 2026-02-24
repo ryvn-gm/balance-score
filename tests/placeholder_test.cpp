de0ca810488033321ecc611520c3af498afe7a9e
ryvn-gm <ryvn.lo@gomore.net>
2026-02-24 14:28:44 +0800

    Add GoogleTest integration with placeholder test

---
+++ tests/placeholder_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+namespace balance_score {
+namespace {
+
+TEST(BalanceScoreSetup, ProjectBuildsAndTestsRun) {
+    EXPECT_TRUE(true);
+}
+
+}  // namespace
+}  // namespace balance_score
